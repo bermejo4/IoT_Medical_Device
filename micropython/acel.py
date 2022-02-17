b2cc12b95a032c3a81082b4a651e5d7fa9b0a163
bermejo4 <minijai72@gmail.com>
2022-02-17 09:37:01 +0100

    acelerometer

---
+++ micropython/acel.py
@@ -0,0 +1,12 @@
+from machine import I2C
+from machine import Pin
+#from machine import sleep
+import utime
+import mpu6050
+i2c = I2C(scl=Pin(22), sda=Pin(21))     #initializing the I2C method for ESP32
+#i2c = I2C(scl=Pin(5), sda=Pin(4))       #initializing the I2C method for ESP8266
+mpu= mpu6050.accel(i2c)
+while True:
+ mpu.get_values()
+ print(mpu.get_values())
+ utimesleep(500)
